e0b08af518bfbd6b49ab08ac1d4ee2122df2fa12
Catherine Johnson <cfjohnson@earthlink.net>
2015-02-16 20:44:10 -0700

    updated with d3.js chart functionality.

---
+++ js/d3-chart.js
@@ -0,0 +1,7 @@
+// var bardata = [20, 30, 45, 15],
+// height = 400,
+// width = 600, 
+// barWidth = 50,
+// barOffset = 5
+
+// d3.select('#chart').append('svg');
--- js/instagram.js
+++ js/instagram.js
@@ -3,13 +3,12 @@
 $(document).ready(function() {
     
         var $access_token = '{access-token}';
-        whatever();
-        whatever('https://www.strava.com/api/v3/athlete/activities?per_page=1&access_token=279b1f1f6d51063f18cf6f1d24e0181167e5bd83&callback=?"');
-        //whatever('https://api.instagram.com/v1/users/self?access_token=1622143161.1fb234f.d77f97d7500b49ed8af9545b05c726ac');
+        stravaRequest('https://www.strava.com/api/v3/athlete/activities?per_page=1&access_token=279b1f1f6d51063f18cf6f1d24e0181167e5bd83&callback=?"');
+        instaRequest('https://api.instagram.com/v1/users/self/feed?access_token=1622143161.1fb234f.d77f97d7500b49ed8af9545b05c726ac');
     
     
 });
-function whatever (next_url, count) {
+function stravaRequest (next_url, count) {
 	$.ajax({
         method: "GET",
         url: next_url,
@@ -22,30 +21,139 @@ function whatever (next_url, count) {
            for (key in newData) {
            		//console.log(key + " : " + newData[key]);
            }
-           setupTemplate(newData);
+           setupStravaTemplate(newData);
         },
         error: function(jqXHR, textStatus, errorThrown) {
         }
     });
 }
 
-function setupTemplate (newData) {
-  
-  var source = $('#title-template').html();
-  var template = Handlebars.compile(source);
-  console.log('new data id' + newData.id);
-  console.log(template);
+function instaRequest (next_url, count) {
+  $.ajax({
+    method: "GET",
+    url: next_url,
+    dataType: 'jsonp',
+    data: {},
+    success: function(data) {
+      var instaData = data.data[0];
+      //console.log(instaData);
 
+      setupInstaTemplate(instaData);
+    }
+  });
+}
+
+function setupStravaTemplate (newData) {
+  
+  var source = $('#strava-template').html();
+  var template = Handlebars.compile(source);
   var context = {
     title: newData.id,
-    avgCadence: data.average_cadence,
-    avgWatts: data.average_watts
+    avgCadence: newData.average_cadence,
+    avgWatts: newData.average_watts,
+    avgSpeed: newData.average_speed
   }
   template(context);
-  $('.template').append(template(context));
-  console.log(newData)
+  $('.strava-template').append(template(context));
+  graphData(newData);
+}
+
+function setupInstaTemplate (newData) {
+  //console.log(newData.data[0]);
   
-  // console.log("average cadence: " + data.average_cadence);
-  // console.log("average speed: " + data.average_speed);
-  // console.log("average watts: " + data.average_watts);
+  var source = $('#insta-template').html();
+  var template = Handlebars.compile(source);
+  var context = {
+    imageUrl: newData.images.standard_resolution.url,
+    imageUrlLink: newData.link
+  }
+  template(context);
+  $('.insta-template').append(template(context));
+  setWidths(newData);
+}
+
+function setWidths (newData) {
+  var width = newData.images.standard_resolution.width - 20;
+  var height = newData.images.standard_resolution.height - 20;
+
+  $('.strava-template').css("height", height + "px");
+  $('.strava-template').css("width", width + "px");
+}
+
+function graphData (newData) {
+  var bardata = [
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    {
+      yHeight: newData.average_speed,
+      name: 'average speed'
+    }
+  ],
+  height = 400,
+  width = 600, 
+  barWidth = 50,
+  barOffset = 5
+
+  heightArray = [];
+  for (var i=0; i < bardata.length; i++) {
+    heightArray.push(bardata[i].yHeight);
+    //console.log(bardata[i].yHeight);
+  }
+  
+  maxHeight = d3.max(heightArray);
+
+  var yScale = d3.scale.linear()
+          .domain([0, d3.max(heightArray)])
+          .range([0, height])
+
+  //.domain(bardata.map(function (d){ return d.name;}))
+
+  var xScale = d3.scale.ordinal()
+
+          .domain(bardata.map(function(bardata) { return bardata.name; }))
+          .domain(d3.range(bardata.length))
+          .rangeBands([0, width], .1)
+
+
+  d3.select('#chart').append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .style('background', 'transparent')
+    .selectAll('rect').data(bardata)
+    .enter().append('rect')
+      .style('fill', '#C61C6F')
+      .attr('width', xScale.rangeBand())
+      .attr('height', function(d) {
+        return yScale(d.yHeight);
+      })
+      .attr('x', function(d, i){
+        console.log(i);
+        console.log(xScale(i));
+        return xScale(i);
+        //return i * (xScale.rangeBand() + barOffset);
+      })
+      .attr('y', function(d) {
+        return height - yScale(d.yHeight);
+      })
+      .text(function(d, i) {
+        return d.name;
+      })
+
+    // .enter().append('text')
+    //   .attr('fill', "red")
+    //   .attr('font-family', "sans-serif")
+    //   .attr('x', function(d, i){
+    //     return i * (barWidth + barOffset);
+    //   })
+    //   .attr('y', function(d) {
+    //     return height - yScale(d.yHeight);
+    //   })
+    //   .text(function(d, i) {
+    //     return d.name;
+    //   })
 }
