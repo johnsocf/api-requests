0e37f20c56351c2015e67bfcb3bcf87ba763cf17
Catherine Johnson <cfjohnson@earthlink.net>
2015-02-22 11:25:48 -0700

    updated instagram.js with animation and margins.

--- js/instagram.js
+++ js/instagram.js
@@ -81,7 +81,233 @@ function setWidths (newData) {
 }
 
 function graphData (newData) {
+  var margin = { top: 30, bottom: 40, left: 50, right: 30 }
   var bardata = [
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    {
+      yHeight: newData.average_speed,
+      name: 'average speed'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    {
+      yHeight: newData.average_speed,
+      name: 'average speed'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    {
+      yHeight: newData.average_speed,
+      name: 'average speed'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    {
+      yHeight: newData.average_speed,
+      name: 'average speed'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    {
+      yHeight: newData.average_speed,
+      name: 'average speed'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    {
+      yHeight: newData.average_speed,
+      name: 'average speed'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    {
+      yHeight: newData.average_speed,
+      name: 'average speed'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    {
+      yHeight: newData.average_speed,
+      name: 'average speed'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    {
+      yHeight: newData.average_speed,
+      name: 'average speed'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    {
+      yHeight: newData.average_speed,
+      name: 'average speed'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    {
+      yHeight: newData.average_speed,
+      name: 'average speed'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    {
+      yHeight: newData.average_speed,
+      name: 'average speed'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    {
+      yHeight: newData.average_speed,
+      name: 'average speed'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    {
+      yHeight: newData.average_speed,
+      name: 'average speed'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    {
+      yHeight: newData.average_speed,
+      name: 'average speed'
+    },
+    { yHeight: newData.average_cadence,
+      name: 'average cadence'
+     },
+    {
+      yHeight: newData.average_watts,
+      name: 'average watts'
+    },
+    {
+      yHeight: newData.average_speed,
+      name: 'average speed'
+    },
     { yHeight: newData.average_cadence,
       name: 'average cadence'
      },
@@ -94,66 +320,139 @@ function graphData (newData) {
       name: 'average speed'
     }
   ],
-  height = 400,
-  width = 600, 
+  height = 400 - margin.top - margin.bottom,
+  width = 600 - margin.left - margin.right, 
   barWidth = 50,
   barOffset = 5
 
   heightArray = [];
   for (var i=0; i < bardata.length; i++) {
     heightArray.push(bardata[i].yHeight);
-    //console.log(bardata[i].yHeight);
   }
+
+  for (var i=0; i < bardata.length; i++) {
+   console.log(bardata[i].yHeight);
+  }
+  bardata.sort(function compareNumbers(a, b) {
+    return a.yHeight - b.yHeight;
+  });
   
   maxHeight = d3.max(heightArray);
+  console.log(bardata.length)
+  var tempColor;
+
+  var colors = d3.scale.linear()
+          // .domain([0, maxHeight])
+          .domain([0, bardata.length])
+          .range(["red", "yellow"]);
 
   var yScale = d3.scale.linear()
           .domain([0, d3.max(heightArray)])
           .range([0, height])
 
-  //.domain(bardata.map(function (d){ return d.name;}))
 
   var xScale = d3.scale.ordinal()
-
           .domain(bardata.map(function(bardata) { return bardata.name; }))
           .domain(d3.range(bardata.length))
-          .rangeBands([0, width], .1)
+          .rangeBands([0, width], .5, 1)
 
+  var tooltip = d3.select('body').append('div')
+          .style('position', 'absolute')
+          .style('top', '0')
+          .style('left', '0')
+          .style('padding', '0 10px')
+          .style('background', 'white')
+          .style('opacity', 0)
 
-  d3.select('#chart').append('svg')
-    .attr('width', width)
-    .attr('height', height)
+  var myChart = d3.select('#chart').append('svg')
+    .style('background', '#E7E0CB')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.left + margin.right)
+    .append('g')
+    .attr('transform', 'translate('+ margin.left + ', ' + margin.top +')')
     .style('background', 'transparent')
     .selectAll('rect').data(bardata)
     .enter().append('rect')
-      .style('fill', '#C61C6F')
-      .attr('width', xScale.rangeBand())
-      .attr('height', function(d) {
-        return yScale(d.yHeight);
+      .style('fill', function(d, i){
+        // return colors(d.yHeight);
+        return colors(i);
       })
+      .attr('width', xScale.rangeBand())
+      .attr('height', 0)
       .attr('x', function(d, i){
-        console.log(i);
-        console.log(xScale(i));
         return xScale(i);
         //return i * (xScale.rangeBand() + barOffset);
       })
-      .attr('y', function(d) {
-        return height - yScale(d.yHeight);
-      })
+      .attr('y', height)
       .text(function(d, i) {
         return d.name;
       })
+    .on('mouseover', function(d) {
+        tooltip.transition()
+          .style('opacity', .9)
 
-    // .enter().append('text')
-    //   .attr('fill', "red")
-    //   .attr('font-family', "sans-serif")
-    //   .attr('x', function(d, i){
-    //     return i * (barWidth + barOffset);
-    //   })
-    //   .attr('y', function(d) {
-    //     return height - yScale(d.yHeight);
-    //   })
-    //   .text(function(d, i) {
-    //     return d.name;
-    //   })
+        tooltip.html(d.name)
+          .style('left', (d3.event.pageX) + 'px')
+          .style('top', (d3.event.pageY) - 30 + 'px')
+
+        tempColor = this.style.fill;
+        d3.select(this)
+          // .transition()
+          .style('opacity', .5)
+          .style('fill', 'yellow')
+    })
+    .on('mouseout', function(d) {
+        d3.select(this)
+        // .transition().delay(500).duration(800)
+          .style('opacity', .9)
+          .style('fill', tempColor);
+    });
+
+  myChart.transition()
+    .attr('height', function(d) {
+      return yScale(d.yHeight);
+    })
+    .attr('y', function(d) {
+      return height - yScale(d.yHeight);
+    })
+    .delay(function(d, i){
+      return i * 10;
+    })
+    .duration(1000)
+    .ease('elastic')
+
+  var vGuideScale = d3.scale.linear()
+    .domain([0, d3.max(heightArray)])
+    .range([height, 0])
+
+  var vAxis = d3.svg.axis()
+    .scale(vGuideScale)
+    .orient('left')
+    .ticks(20)
+
+  var vGuide = d3.select('svg').append('g')
+      vAxis(vGuide)
+
+      vGuide.attr('transform', 'translate('+ margin.left + ', ' + margin.top + ')')
+      vGuide.selectAll('path')
+            .style({fill: 'none', stroke: "#000"})
+      vGuide.selectAll('line')
+            .style({stroke: "#000"})
+
+  var hAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient('bottom')
+      .tickValues(xScale.domain().filter(function(d, i) {
+        return !(i % (bardata.length/5));
+      }))
+
+  var hGuide = d3.select('svg').append('g')
+      hAxis(hGuide)
+
+      hGuide.attr('transform', 'translate('+ (margin.left - 0) + ', ' + (height + margin.top) + ')')
+      hGuide.selectAll('path')
+            .style({fill: 'none', stroke: "#000"})
+      hGuide.selectAll('line')
+            .style({stroke: "#000"})
+
 }
